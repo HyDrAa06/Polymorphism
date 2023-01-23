a6f5becc604c0a9345005953020967566da1292a
HyDrAa06 <108666340+HyDrAa06@users.noreply.github.com>
2023-01-23 15:30:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Vehicles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(0, 0, 0);
+            Truck truck = new Truck(0, 0, 0);
+            Bus bus = new Bus(0, 0, 0);
+
+            Console.WriteLine("Fuel quantity, consumption and tank capacity:");
+
+            string[] info = Console.ReadLine().Split().ToArray();
+            if (double.Parse(info[1]) > double.Parse(info[3]))
+            {
+                car = new Car(0, double.Parse(info[2]), double.Parse(info[3]));
+            }
+            else
+            {
+                car = new Car(double.Parse(info[1]), double.Parse(info[2]), double.Parse(info[3]));
+            }
+            
+            info = Console.ReadLine().Split().ToArray();
+            if (double.Parse(info[1]) > double.Parse(info[3]))
+            {
+                truck = new Truck(0, double.Parse(info[2]), double.Parse(info[3]));
+            }
+            else
+            {
+                truck = new Truck(double.Parse(info[1]), double.Parse(info[2]), double.Parse(info[3]));
+            }
+
+            info = Console.ReadLine().Split().ToArray();
+            if (double.Parse(info[1]) > double.Parse(info[3]))
+            {
+                bus = new Bus(0, double.Parse(info[2]), double.Parse(info[3]));
+            }
+            else
+            {
+                bus = new Bus(double.Parse(info[1]), double.Parse(info[2]), double.Parse(info[3]));
+            }
+
+
+            Console.WriteLine("Number of commands");
+            int number = int.Parse(Console.ReadLine());
+
+            for(int i =0;i< number; i++)
+            {
+                string[] command = Console.ReadLine().Split().ToArray();
+
+                if(command[0] == "Drive")
+                {
+                    if (command[1] == "Car")
+                    {
+                        car.Drive(double.Parse(command[2]));
+                    }
+                    if(command[1] == "Truck")
+                    {
+                        truck.Drive(double.Parse(command[2]));
+                    }
+                    if (command[1]=="Bus")
+                    {
+                        bus.Drive(double.Parse(command[2]));
+                    }
+                }
+
+                if (command[0] == "DriveEmpty")
+                {
+                    bus.DriveEmpty(double.Parse(command[2]), double.Parse(info[2]));
+                }
+
+                if(command[0] == "Refuel")
+                {
+                    if (command[1] == "Car")
+                    {
+                        car.Refuel(double.Parse(command[2]));
+                    }
+                    if (command[1] == "Truck")
+                    {
+                        truck.Refuel(double.Parse(command[2]));
+                    }
+                    if (command[1] == "Bus")
+                    {
+                        bus.Refuel(double.Parse(command[2]));
+                    }
+                }
+            }
+
+            car.Print();
+            truck.Print();
+            bus.Print();
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private double fuelCapacity;
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public double FuelCapacity
+        {
+            get { return fuelCapacity; }
+            set { fuelCapacity = value; }
+        }
+
+        public Vehicle(double fuelQuantity, double fuelConsumption, double fuelCapacity)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+            this.FuelCapacity = fuelCapacity;
+        }
+
+        public abstract void Drive(double distance);
+        public abstract void Refuel(double liters);
+
+        public abstract void Print();
+    }
+
+    public class Car : Vehicle
+    {
+        public Car(double fuelQuantity, double fuelConsumption, double fuelCapacity) : base(fuelQuantity, fuelConsumption, fuelCapacity)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption + 0.9;
+            this.FuelCapacity = fuelCapacity;
+        }
+
+        public override void Drive(double distance)
+        {
+            if (FuelQuantity >= FuelConsumption * distance)
+            {
+                FuelQuantity = FuelQuantity - FuelConsumption * distance;
+                Console.WriteLine($"Car travelled {distance}km");
+            }
+            else
+            {
+                Console.WriteLine("Car needs refueling");
+            }
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Car: {FuelQuantity:f2}");
+        }
+
+        public override void Refuel(double liters)
+        {
+            if(liters > 0)
+            {
+                if(FuelCapacity >= FuelQuantity + liters)
+                {
+                    FuelQuantity = FuelQuantity + liters;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot fit {liters} fuel in the tank");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fuel must be positive number.");
+            }
+            
+        }
+    }
+
+    public class Truck : Vehicle
+    {
+        public Truck(double fuelQuantity, double fuelConsumption, double fuelCapacity) : base(fuelQuantity, fuelConsumption, fuelCapacity)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption + 1.6;
+            this.FuelCapacity = fuelCapacity;
+        }
+        public override void Drive(double distance)
+        {
+            if (FuelQuantity >= FuelConsumption * distance)
+            {
+                FuelQuantity = FuelQuantity - FuelConsumption * distance;
+                Console.WriteLine($"Truck travelled {distance}km");
+            }
+            else
+            {
+                Console.WriteLine("Truck needs refueling");
+            }        
+        }
+        public override void Refuel(double liters)
+        {
+            if (liters > 0)
+            {
+                if (FuelCapacity >= FuelQuantity + liters*0.95)
+                {
+                    FuelQuantity = FuelQuantity + liters*0.95;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot fit {liters} fuel in the tank");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fuel must be positive number.");
+            }
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Truck: {FuelQuantity:f2}");
+        }
+    }
+
+    public class Bus : Vehicle
+    {
+        public Bus(double fuelQuantity, double fuelConsumption, double fuelCapacity) : base(fuelQuantity, fuelConsumption, fuelCapacity)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption + 1.4;
+            this.FuelCapacity = fuelCapacity;
+        }
+
+        public override void Drive(double distance)
+        {
+            if (FuelQuantity >= FuelConsumption * distance)
+            {
+                FuelQuantity = FuelQuantity - FuelConsumption * distance;
+                Console.WriteLine($"Bus travelled {distance}km");
+            }
+            else
+            {
+                Console.WriteLine("Bus needs refueling");
+            }
+        }
+
+        public void DriveEmpty(double distance, double fuelConsumption)
+        {
+            this.FuelConsumption = fuelConsumption;
+            if (FuelQuantity >= FuelConsumption * distance)
+            {
+                FuelQuantity = FuelQuantity - FuelConsumption * distance;
+                Console.WriteLine($"Bus travelled {distance}km");
+            }
+            else
+            {
+                Console.WriteLine("Bus needs refueling");
+            }
+            this.FuelConsumption = fuelConsumption*1.4;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Bus: {FuelQuantity:f2}");
+        }
+
+        public override void Refuel(double liters)
+        {
+            if (liters > 0)
+            {
+                if (FuelCapacity >= FuelQuantity + liters)
+                {
+                    FuelQuantity = FuelQuantity + liters;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot fit {liters} fuel in the tank");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fuel must be positive number.");
+            }
+        }
+    }
+}
